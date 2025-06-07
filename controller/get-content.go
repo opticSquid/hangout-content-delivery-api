ee62537b278051256011d9097bbbc2620902e1d5
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-06-07 18:29:44 +0530

    setting up open telemetry

--- controller/get-content.go
+++ controller/get-content.go
@@ -1,7 +1,43 @@
 package controller
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/otel"
+	"hangoutsb.in/hangout-content-delivery-api/model"
+)
+
+const name = "hangoutsb.in/hangout-content-delivery-api/controller"
+
+var (
+	tracer = otel.Tracer(name)
+	meter  = otel.Meter(name)
+	logger = otelslog.NewLogger(name)
+)
 
 func GetContent(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracer.Start(r.Context(), "controller")
+	defer span.End()
+	log.Info().Str("Path", r.Pattern).Str("Method", r.Method).Msg("recieved request")
+	// Only allow GET requests
+	if r.Method != http.MethodGet {
+		log.Error().Str("Path", r.Pattern).Str("Method", r.Method).Msg("method not allowed in path")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	// Set the content type to JSON
+	w.Header().Set("Content-Type", "application/json")
 
+	// Create a response object
+	resp := model.Response{
+		Message: "Hello, JSON!",
+		Status:  "success",
+	}
+	// Encode response to JSON and write to client
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+	}
 }
---
+++ model/response.go
@@ -0,0 +1,7 @@
+package model
+
+// Define a struct for the JSON response
+type Response struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
