57c1870b1521f1329c4112f92c21be1ffd844f5a
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-09-06 17:09:46 +0530

    Able to generate signed cookies but further modification and testing required

---
+++ aws/presigned-cookies.go
@@ -0,0 +1,66 @@
+package aws
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/feature/cloudfront/sign"
+	"github.com/knadh/koanf/v2"
+	"github.com/rs/zerolog/log"
+)
+
+func GeneratePreSignedCookies(dirName string, k *koanf.Koanf) ([]*http.Cookie, error) {
+
+	privateKeyPath := k.String("aws.video.cloudfront.privateKeyPath")
+	privateKeyBytes, err := os.ReadFile(privateKeyPath)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to read private key")
+		return nil, fmt.Errorf("failed to read private key: %w", err)
+	}
+	log.Debug().Msg("successfully read private key")
+
+	privateKey, err := parsePrivateKey(privateKeyBytes)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to parse private key")
+		return nil, fmt.Errorf("failed to parse private key: %w", err)
+	}
+	log.Debug().Msg("successfully parsed private key")
+
+	publicKeyId := k.String("aws.video.cloudfront.publicKeyId")
+	expiresAt := time.Now().Add(time.Duration(k.Int("aws.video.cloudfront.expirationDurationInSeconds")) * time.Second)
+	resource := fmt.Sprintf("http*://%s/%s/*", k.String("aws.video.cloudfront.domain"), dirName)
+
+	log.Debug().Msg("starting to generate cookies")
+	cookies, err := sign.NewCookieSigner(publicKeyId, privateKey).Sign(resource, expiresAt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate cookies: %w", err)
+	}
+	log.Debug().Msg("successfully generated cookies")
+
+	return cookies, nil
+}
+
+// parsePrivateKey parses a PEM-encoded RSA private key.
+func parsePrivateKey(pemBytes []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(pemBytes)
+	if block == nil || block.Type != "PRIVATE KEY" {
+		return nil, fmt.Errorf("failed to decode PEM block containing RSA private key")
+	}
+
+	log.Debug().Msg("Attempting to parse private key as PKCS#8")
+	privateKeyInterface, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse PKCS#8 private key: %w", err)
+	}
+
+	rsaKey, ok := privateKeyInterface.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("parsed PKCS#8 private key is not an RSA private key (got %T)", privateKeyInterface)
+	}
+	return rsaKey, nil
+}
--- controller/get-video.go
+++ controller/get-video.go
@@ -1,7 +1,44 @@
 package controller
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+	"hangoutsb.in/hangout-content-delivery-api/aws"
+)
 
 func (config *ControllerConfig) GetVideo(w http.ResponseWriter, r *http.Request) {
+	log.Info().Str("Path", r.Pattern).Str("Method", r.Method).Str("file", r.PathValue("video_id")).Msg("recieved request")
+	// Only allow GET requests
+	if r.Method != http.MethodGet {
+		log.Error().Str("Path", r.Pattern).Str("Method", r.Method).Msg("method not allowed in path")
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fileName := r.PathValue("video_id")
+	preSignedCookies, err := aws.GeneratePreSignedCookies(getDirName(fileName), config.appConfig)
+	if err != nil {
+		log.Error().Err(err).Str("video file", fileName).Msg("could not generate cookies for the given file")
+		writeProblemDetails(w, http.StatusInternalServerError, "Failed to generate Presigned cookies", "Could not generate presigned cookies for the given file. Make sure the filename is valid", "https://httpstatuses.com/500", r.URL.Path)
+		return
+	}
+	for _, c := range preSignedCookies {
+		http.SetCookie(w, c)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(`{"message":"Signed cookies set successfully"}`))
+
+}
+
+func getDirName(filename string) string {
+	fName, _, found := strings.Cut(filename, ".")
+	if found {
+		return fName
+	} else {
+		return filename
+	}
 
 }
