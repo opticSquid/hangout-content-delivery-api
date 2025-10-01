2fe2e5ffb08ad47f801980ed6ca4520bcc5304d6
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2025-10-01 22:31:17 +0530

    added options and head in allowed methods

--- router/add-headers.go
+++ router/add-headers.go
@@ -9,7 +9,8 @@ import (
 func withHeaders(h http.HandlerFunc, k *koanf.Koanf) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", k.String("cors.allowed-origins")) // Or set to a specific origin
-		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		// TODO: in future only keep HEAD for presigned cookies no need for body in presigned cookies
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		h(w, r)
 	}
